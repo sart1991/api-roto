0e9b77482abb0cbebf97ff593c74d9c78a82c5e4
Sergio Rojas <sart1991@gmail.com>
2017-06-05 12:09:22 -0500

    first commit test

---
+++ index.js
@@ -0,0 +1,11 @@
+// Load the http module to create an http server.
+const http = require('http');
+
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.get("/", (req, res) => res.json({status: "NTask API"}))
+
+app.listen(PORT, () => console.log(`NTask API - PORT ${PORT}`));
